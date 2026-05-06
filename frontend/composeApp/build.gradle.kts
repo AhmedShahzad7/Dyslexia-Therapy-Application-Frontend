6f96eb135255ca1070010f14b6cdda56412f6a76
iMuhammad-pk <muhammadmasood1557@gmail.com>
2026-05-07 00:28:32 +0500

    Added Bouncy Screen and 1st Level 1 personalized Question 1

--- frontend/composeApp/build.gradle.kts
+++ frontend/composeApp/build.gradle.kts
@@ -30,6 +30,8 @@ kotlin {
             implementation("com.squareup.retrofit2:retrofit:2.9.0")
             implementation("com.squareup.retrofit2:converter-gson:2.9.0")
             implementation("com.google.code.gson:gson:2.10.1")
+            implementation("androidx.media3:media3-exoplayer:1.2.1")
+            implementation("androidx.media3:media3-ui:1.2.1")
 
 
             implementation(libs.androidx.credentials)
